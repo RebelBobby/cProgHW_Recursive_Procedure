9aac144707e481ec82ec919c88d508e9692eb024
Joseph Matulewicz <joseph.matulewicz@gmail.com>
2022-06-25 17:51:09 -0700

    Recursive Procedure

--- jMatulewicz.c
+++ jMatulewicz.c
@@ -1,9 +1,26 @@
+/*JMatulewicz
+June 25, 2022
+C prgm Recursive Procedure Matuelwicz to zciwleutaM*/
 #include <stdio.h>
 
-int
-main (void)
-{
-  printf ("Hello, world!\n");
-  return 0;
+void reverse(char *str, int j, int k){
+  char temp;
+  if (j<k) {
+    temp = str[j];
+    str[j] = str[k];
+    str[k] = temp;
+    reverse(str, j+1, k-1);
+  }//ra2
 }
+  int main(){
+    char word[11] = "Matulewicz";
+    printf("%s    ", word);
+    reverse(word, 0, 9);
+    printf("%s    ", word); //ral
+    return 0;
+  }
 
+  /*
+  Test Run
+  Matulewicz    zciwelutaM 
+  */
